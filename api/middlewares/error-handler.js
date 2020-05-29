d3cf56a60f332b7969572ce89216e7b988f2cb27
Matus Gallik <matusgallik008@gmail.com>
2020-05-29 21:48:00 +0100

    Save

--- api/middlewares/error-handler.js
+++ api/middlewares/error-handler.js
@@ -6,28 +6,28 @@ function errorHandler(err, req, res, next) {
     if (typeof (err) === 'string') {
         // custom application error
         l.err(err.message);
-        return res.status(400).json({ message: err });
+        return res.status(400).json({message:  "400" + err});
     }
 
-    if (err.name ==='NoKeyInDB'){
+    if (err.name === 'NoKeyInDB') {
         l.err(err.message);
-        return res.status(500).json({ message: "Internal server error! Please contact admin." });
+        return res.status(500).json({message: "Internal server error! Please contact admin."});
     }
 
     if (err.name === 'ValidationError') {
         // mongoose validation error
         l.err(err.message);
-        return res.status(400).json({ message: err.message });
+        return res.status(400).json({message: "400" + err.message});
     }
 
     if (err.name === 'UnauthorizedError') {
         // jwt authentication error
-        l.err( err.message);
-        return res.status(401).json({ message: 'Invalid Token or unknown URL' });
+        l.err(err.message);
+        return res.status(401).json({message: 'Invalid Token or unknown URL'});
 
     }
     l.err(err.message);
 
     // default to 500 server error
-    return res.status(500).json({ message: err.message });
+    return res.status(500).json({message: err.message});
 }
--- main.js
+++ main.js
@@ -6,13 +6,18 @@ const bodyParser = require('body-parser');
 const jwt = require('api/middlewares/jwt');
 const errorHandler = require('api/middlewares/error-handler');
 const config = require('config/config.json');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const socketioJwt = require('socketio-jwt');
+const l = require("utils/logging")
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
 // use JWT auth to secure the api
 app.use(jwt());
+
 
 // api routes
 app.use('/users', require('./api/routes/users.controller'));
@@ -22,7 +27,19 @@ app.use('/location', require('./api/routes/userLocation.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : ( config.PORT || 4000);
-const server = app.listen(port, function () {
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : (config.PORT || 4000);
+
+// set authorization for socket.io
+io.sockets //TODO: double check the security of JWT here and whether it can be spoofed
+    .on('connection', socketioJwt.authorize({
+        secret: config.secret,
+        timeout: 5000 // 15 seconds to send the authentication message
+    }))
+    .on('authenticated', (socket) => {
+        //this socket is authenticated, we are good to handle more events from it.
+        l.log(`User id ${socket.decoded_token.sub} joined the game`);
+    });
+
+const server = http.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
