7ae1292ddcb4f807f407b4b40618bb4b17cc9acd
macutko <matusgallik00@gmail.com>
2020-06-03 19:15:27 +0200

    adding user sockets

--- main.js
+++ main.js
@@ -6,18 +6,13 @@ const bodyParser = require('body-parser');
 const jwt = require('api/middlewares/jwt');
 const errorHandler = require('api/middlewares/error-handler');
 const config = require('config/config.json');
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const socketioJwt = require('socketio-jwt');
-const l = require("utils/logging")
-
+const l = require('utils/logging')
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
 // use JWT auth to secure the api
 app.use(jwt());
-
 
 // api routes
 app.use('/users', require('./api/routes/users.controller'));
@@ -28,18 +23,21 @@ app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : (config.PORT || 4000);
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const socketioJwt = require('socketio-jwt');
 
-// set authorization for socket.io
-io.sockets //TODO: double check the security of JWT here and whether it can be spoofed
-    .on('connection', socketioJwt.authorize({
-        secret: config.secret,
-        timeout: 5000 // 15 seconds to send the authentication message
-    }))
-    .on('authenticated', (socket) => {
-        //this socket is authenticated, we are good to handle more events from it.
-        l.log(`User id ${socket.decoded_token.sub} joined the game`);
-    });
+io.on('connection', socketioJwt.authorize({
+    secret: config.secret,
+    pingInterval: 10000,
+    pingTimeout: 3000,
+    timeout: 15000 // 15 seconds to send the authentication message
+})).on('authenticated', function (socket) {
+    //this socket is authenticated, we are good to handle more events from it.
+    require('api/routes/IO_position.controller')(socket,io)
+    require('api/routes/io.controller')(socket)
+    l.log('User id connected ' + socket.decoded_token.sub);
+})
 
-const server = http.listen(port, function () {
-    console.log('Server listening on port ' + port);
-});
+server.listen(port, () => console.log("server running on port:" + port));
+
